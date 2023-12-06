817ecfb79b1392937aa3d56dace3750dcfdf4499
Rishabh Rathore <143699401+rishraws@users.noreply.github.com>
2023-12-06 18:11:00 +0530

    Update map.js

--- public/js/map.js
+++ public/js/map.js
@@ -6,7 +6,7 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
   container: "map", // container ID
   // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
-  style: "mapbox://styles/mapbox/streets-v12", // style URL
+  style: "mapbox://styles/mapbox/standard", // style URL
   center: listing.geometry.coordinates, // starting position [lng, lat]
   zoom: 9, // starting zoom
 });
